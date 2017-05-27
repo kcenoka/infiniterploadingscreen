bac517bdcbc364aadbcb2aeb5c9b8951b4270348
kcenoka <kcenoka@gmail.com>
2017-05-27 19:52:06 +1200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -80,10 +80,10 @@ var l_bgDarkening = 50;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
-var l_music = true;
+var l_music = false;
 
 /*Display actual song's name?*/
-var l_musicDisplay = true;
+var l_musicDisplay = false;
 
 /*Music playlist
 Place how much .ogg songs you want
